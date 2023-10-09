30bad14d9edf2795a847a05b26b6666afb000d95
Mohammad Usman <uk351675@gmail.com>
2023-10-10 01:10:49 +0530

    API most reusable ,mor e flexibility

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const starContainer = {
   display: "flex",
   alignItems: "center",
@@ -6,22 +8,94 @@ const starContainer = {
 
 const star = {
   display: "flex",
-  gap: "3px",
-};
-const rateText = {
-  margin: "0",
-  lineHeight: "1",
 };
 
-export default function StarRating({ maxRating = 5 }) {
+export default function StarRating({
+  maxRating = 5,
+  color = "#fcc432",
+  size = 30,
+  ratingDefault = 3,
+  className = "",
+  messages = [],
+  setState,
+}) {
+  const [rate, setRate] = useState(ratingDefault);
+  const [hover, setHover] = useState(0);
+  function handleRate(rating) {
+    setRate(rating);
+    setState(rating);
+  }
+
+  const rateText = {
+    margin: "0",
+    lineHeight: "1",
+    color,
+    fontSize: `${size / 2 + 5}px`,
+  };
+
   return (
-    <div style={starContainer}>
+    <div style={starContainer} className={className}>
       <div style={star}>
         {Array.from({ length: maxRating }, (_, i) => (
-          <span>S{i + 1}</span>
+          <Stars
+            key={i}
+            onRate={() => handleRate(i + 1)}
+            full={hover ? hover >= i + 1 : rate >= i + 1}
+            handleIn={() => setHover(i + 1)}
+            handleOut={() => setHover(0)}
+            color={color}
+            size={size}
+          />
         ))}
       </div>
-      <div style={rateText}>rating</div>
+      <p style={rateText}>
+        {messages.length === maxRating
+          ? messages[hover ? hover - 1 : rate - 1]
+          : hover || rate || ""}
+      </p>
     </div>
+  );
+}
+
+function Stars({ onRate, full, handleIn, handleOut, color, size }) {
+  const starStyle = {
+    width: `${size}px`,
+    height: `${size}px`,
+    cursor: "pointer",
+    display: "block",
+  };
+  return (
+    <span
+      role="button"
+      style={starStyle}
+      onClick={onRate}
+      onMouseEnter={handleIn}
+      onMouseLeave={handleOut}
+    >
+      {full ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill={color}
+          stroke={color}
+        >
+          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+        </svg>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke={color}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="{2}"
+            d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+          />
+        </svg>
+      )}
+    </span>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 // import App from "./App";
 
 import StarRating from "./StarRating";
 
+function Test() {
+  const [times, setTimes] = useState(0);
+  return (
+    <div>
+      <StarRating setState={setTimes} />
+      <p>This is the game of perseverence,Love you {times} times.</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={10} />
-    <StarRating maxRating={5} />
-    <StarRating />
+    <StarRating maxRating={5} color="violet" size={20} />
+    <StarRating maxRating={5} color="indigo" ratingDefault={4} />
+    <StarRating maxRating={4} color="blue" />
+    <StarRating
+      maxRating={5}
+      color="green"
+      messages={["terrible", "bad", "ok", "good", "amazing"]}
+    />
+    <StarRating
+      maxRating={3}
+      color="yellow"
+      ratingDefault={0}
+      messages={["terrible", "bad", "ok", "good", "amazing"]}
+    />
+    <Test />
   </React.StrictMode>
 );
